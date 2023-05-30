0f81dc2003b975d974182edbcb37b6a9548ec017
Teych <tholub99@gmail.com>
2023-05-29 22:09:27 -0400

    Add email compatibility

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
 function sendEmail()
 {
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "tholub99@gmail.com",
-        Password : "36E7A5A74AD4AEF2B073674872B8720E9C51",
-        To : 'tholub99@gmail.com',
-        From : document.getElementById("Email").value,
-        Subject : document.getElementById("Subject").value,
-        Body : `Name: ${document.getElementById("Name").value}\n
-        Company: ${document.getElementById("Company").value}\n
+        SecureToken: "e5c14316-c8d6-4c95-9fbc-1a3cd3391148",
+        To : 'sales@cogitascience.com',
+        From : "contact@cogitascience.com",
+        Subject : `New Contact Inquiry: ${document.getElementById("Subject").value}`,
+        Body : `Name: ${document.getElementById("Name").value}<br>
+        From: ${document.getElementById("Email").value}<br>
+        Company: ${document.getElementById("Company").value}<br>
+        Subject: ${document.getElementById("Subject").value}<br>
         Message: ${document.getElementById("Message").value}`
     }).then(
     message => alert(message)
